0a19e03ce940d4976aef1e9f0d892548d0190b14
Sugandha123 <sugandhasharma555@gmail.com>
2022-01-10 12:42:42 +0530

    adding in homepage

--- src/component/homePage.js
+++ src/component/homePage.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
 
 const HomePage = () => {
 
     return (
-        <div>CLick to Search Address</div>
+        <div className='pageContainer pageHeader'>
+
+            <div className='pageContent row'>
+                <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+                    <Link to='/searchAddress'>Search Address</Link>
+                </div>
+
+                <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+                    <Link to='/transferList'>Transfer List</Link>
+                </div>
+
+            </div>
+        </div>
 
     );
 };
